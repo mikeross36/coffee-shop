ad5f021ad8b055759295459291963eca06b2b227
Mike Ross <mikeross36mikeross@gmail.com>
2022-09-07 14:48:17 +0200

    #sl5364

--- app/js/displaySlides.js
+++ app/js/displaySlides.js
@@ -1,5 +1,15 @@
 "use strict"
 
+const globals = () => {
+    const containers = document.querySelectorAll(".slide__container")
+    const slides = [...document.querySelectorAll(".slide__container")]
+    const prevBtn = document.querySelector(".prev__slide")
+    const nextBtn = document.querySelector(".next__slide")
+    let currSlide = 0;
+    return {containers, slides, prevBtn, nextBtn, currSlide}
+};
+let {containers, slides, prevBtn, nextBtn, currSlide} = globals()
+
 class Testimonials {
     getSlides = async () => {
         try {
@@ -13,36 +23,64 @@ class Testimonials {
     }
 
     displaySlides = slides => {
-        const containers = document.querySelectorAll(".slide__container")
-        containers.forEach((container, cIdx) => {
-            slides.forEach((slide, sIdx) => {
-                if (cIdx === sIdx) {
-                    container.innerHTML = `
-                    <div class="slide">
-                        <img src="${slide.quoteIcon}" alt="" class="slide__quote" width="30px" height="30px">
-                        <div class="user">
-                            <img src="${slide.image}" alt="" class="user__img">
-                            <div class="user__info">
-                                <h3>${slide.name}</h3>
-                                <div class="user__stars">
-                                    <img src="${slide.starIcon}" alt="" class="star" width="20px" height="20px">
-                                    <img src="${slide.starIcon}" alt="" class="star" width="20px" height="20px">
-                                    <img src="${slide.starIcon}" alt="" class="star" width="20px" height="20px">
-                                    <img src="${slide.starIcon}" alt="" class="star" width="20px" height="20px">
-                                    <img src="${slide.starIcon}" alt="" class="star" width="20px" height="20px">
+        if (containers) {
+            containers.forEach((container, cIdx) => {
+                slides.forEach((slide, sIdx) => {
+                    if (cIdx === sIdx) {
+                        container.innerHTML = `
+                        <div class="slide">
+                            <img src="${slide.quoteIcon}" alt="" class="slide__quote" width="30px" height="30px">
+                            <div class="user">
+                                <img src="${slide.image}" alt="" class="user__img">
+                                <div class="user__info">
+                                    <h3>${slide.name}</h3>
+                                    <div class="user__stars">
+                                        <img src="${slide.starIcon}" alt="" class="star" width="20px" height="20px">
+                                        <img src="${slide.starIcon}" alt="" class="star" width="20px" height="20px">
+                                        <img src="${slide.starIcon}" alt="" class="star" width="20px" height="20px">
+                                        <img src="${slide.starIcon}" alt="" class="star" width="20px" height="20px">
+                                        <img src="${slide.starIcon}" alt="" class="star" width="20px" height="20px">
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <p class="user__description">${slide.description}</p>
-                    </div> `
-               }
+                            <p class="user__description">${slide.description}</p>
+                        </div> `
+                   }
+                })
             })
-        })
-    };
+        }
+    }
+
+    removeActive = (item, element) => {
+        item[element].classList.remove("active-slide")
+    }
+
+    addActive = (item, element) => {
+        item[element].classList.add("active-slide")
+    }
+
+    setupSlides = () => {
+        if (nextBtn) {
+            nextBtn.onclick = () => {
+                this.removeActive(slides, currSlide)
+                currSlide = (currSlide + 1) % slides.length
+                this.addActive(slides, currSlide)
+            }
+        }
+
+        if (prevBtn) {
+            prevBtn.onclick = () => {
+                this.removeActive(slides, currSlide)
+                currSlide = (currSlide - 1 + slides.length) % slides.length
+                this.addActive(slides, currSlide)
+            }
+        }
+    }
 };
 
 (async () => {
     const testimonials = new Testimonials()
     const slides = await testimonials.getSlides()
     testimonials.displaySlides(slides)
+    testimonials.setupSlides()
 })();
--- app/js/script.js
+++ app/js/script.js
@@ -11,30 +11,7 @@ function scrollHeader() {
     }
 };
 
-function setupSlides() {
-    const slides = [...document.querySelectorAll(".slide__container")]
-    const prevBtn = document.querySelector(".prev__slide")
-    const nextBtn = document.querySelector(".next__slide")
-    let index = 0
-
-    const nextSlide = () => {
-        slides[index].classList.remove("active-slide")
-        index = (index + 1) % slides.length
-        slides[index].classList.add("active-slide")
-    }
-
-    const prevSlide = () => {
-        slides[index].classList.remove("active-slide")
-        index = (index - 1 + slides.length) % slides.length
-        slides[index].classList.add("active-slide")
-    }
-
-    prevBtn.addEventListener("click", prevSlide)
-    nextBtn.addEventListener("click", nextSlide)
-};
-
 (function () {
     window.addEventListener("scroll", scrollHeader);
-    setupSlides()
 })()
 
